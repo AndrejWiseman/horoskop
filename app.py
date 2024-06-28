a0ff29ead265ba575400e6730d38222032279a1a
Andrej <533737@gmail.com>
2024-06-28 09:04:36 +0200

    2

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return '<h1>Zdravo svete!</h1>'
+config = {
+    "DEBUG": True  # run app in debug mode
+}
+
+# Flask to use the above defined config
+app.config.from_mapping(config)
+
+#
+# @app.route('/')
+# def hello_world():  # put application's code here
+#     return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+
+#
+# if __name__ == '__main__':
+#     app.run()
---
+++ main.py
@@ -0,0 +1,25 @@
+from app import app
+from flask import Flask, render_template, url_for
+
+from horoskop import blizanci
+
+
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return render_template('index.html')
+
+@app.route('/blizanci')
+def bliz():
+    return blizanci()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
