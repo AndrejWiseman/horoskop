266ba53df5f38032dace1b1f24748ee332a3458c
Andrej <533737@gmail.com>
2024-06-28 11:25:50 +0200

    55

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template
+from horoskop import blizanci
+
 
 app = Flask(__name__)
 
 
-config = {
-    "DEBUG": True  # run app in debug mode
-}
 
-# Flask to use the above defined config
-app.config.from_mapping(config)
-
-#
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return render_template('index.html')
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
 
 
 
-#
-# if __name__ == '__main__':
-#     app.run()
+@app.route('/blizanci')
+def bliz():
+    return blizanci()
+
+
+
+if __name__ == "__main__":
+    app.run()
--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from app import app
-from flask import render_template
+# from app import app
+# from flask import render_template
 
-from horoskop import blizanci
-
-
-
-
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+# from horoskop import blizanci
 
 
 
-@app.route('/blizanci')
-def bliz():
-    return blizanci()
+
+# @app.route('/')
+# def hello_world():
+#     return render_template('index.html')
 
 
+
+# @app.route('/blizanci')
+# def bliz():
+#     return blizanci()
 
 
 
 
 
 
-if __name__ == "__main__":
-    app.run()
+
+
+# if __name__ == "__main__":
+#     app.run()
